bc6ace32e97c5e809c25aae65fe140adc2c16104
Morten Jensen <mo_ten@hotmail.com>
2025-03-19 21:42:49 +0100

    update

--- LibraryMoney-SP2/src/Title.java
+++ LibraryMoney-SP2/src/Title.java
@@ -1,6 +1,5 @@
 public abstract class Title {
     private String title;
-    private double literatureTypePoints; //Ændret fra int til double så den kan returne i convert
     protected int copies;
     protected double rate;
     private String literatureType;
@@ -12,29 +11,9 @@ public abstract class Title {
         this.rate = 0.067574;
         this.literatureType = literatureType;
 
-        switch (literatureType) {
-            case "BI":
-                this.literatureTypePoints = 3.0;
-                break;
-            case "TE":
-                this.literatureTypePoints = 3.0;
-                break;
-            case "LYRIK":
-                this.literatureTypePoints = 6.0;
-                break;
-            case "SKØN":
-                this.literatureTypePoints = 1.7;
-                break;
-            case "FAG":
-                this.literatureTypePoints = 1.0;
-                break;
-            default:
-                System.out.println("Advarsel: Ukendt litteraturtype " + literatureType);
-                this.literatureTypePoints = -1;
-                break;
-        }
 
     }
+
 
     public double calculateRoyalty() {
         return calculatePoints() * rate;
@@ -43,7 +22,20 @@ public abstract class Title {
     protected abstract double calculatePoints();
 
     protected double convertLiterature() {
-        return literatureTypePoints;
+        switch (literatureType) {
+            case "BI":
+            case "TE":
+                return 3.0;
+            case "LYRIK":
+                return 6.0;
+            case "SKØN":
+                return 1.7;
+            case "FAG":
+                return 1.0;
+            default:
+                System.out.println("Advarsel: Ukendt litteraturtype " + literatureType);
+                return -1;
+        }
     }
 }
 
